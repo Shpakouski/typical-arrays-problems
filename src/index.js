b30f90b11d647a158be6e52376b918e4cee2ba8d
Shpakouski <job.shpakouski@gmail.com>
2021-03-15 23:21:45 +0300

    task is completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
+    let min = array[0];
+    for (let num of array) {
+        if (num < min) {
+            min = num;
+        }
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
+    let max = array[0];
+    for (let num of array) {
+        if (num > max) {
+            max = num;
+        }
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
+    let total = 0;
+    for (let num of array) {
+        total += num;
+    }
+    return total / array.length;
 }
